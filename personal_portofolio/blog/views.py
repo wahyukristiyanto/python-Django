177a63a0ec3db6878b55ba0207fa05b06f34c508
Wahyu Kristiyanto <wahyukristiyanto24@gmail.com>
2023-02-07 21:08:48 +0700

    Learn about redirecting page

--- personal_portofolio/blog/views.py
+++ personal_portofolio/blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 # Create your views here.
 def all_blogs(request):
     projects = Project.objects.all()
+    # projects = Project.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'projects':projects})
+
+def details(request, blog_id):
+    blog = get_object_or_404(Project, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog':blog})
